1c603f2126e5123ad7923b2b65dcf5d338598f55
Mark Molina <prive@markmolina.nl>
2020-03-29 19:18:55 +0200

    Added Parameter singleton for storing parameters

---
+++ covid-19/Singletons/DDParameterStore.swift
@@ -0,0 +1,14 @@
+//
+//  DDParameterStore.swift
+//  covid-19
+//
+//  Created by Mark Anthony Molina on 29/03/2020.
+//  Copyright © 2020 dakke dak. All rights reserved.
+//
+
+import Foundation
+
+class DDParameterStore {
+    
+    static let shared = DDParameterStore()
+}
